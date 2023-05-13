84d28dc9b11e1c846d792a188ec47b19ebacd9fb
nhamonin <nazarii.hamonin@gmail.com>
2023-05-13 22:45:54 +0300

    Add schema validation to POST /feed/post route in feed.js

--- backend/routes/feed.js
+++ backend/routes/feed.js
@@ -4,5 +4,20 @@ export async function feedRoutes(fastify, options) {
   // GET /feed/posts
   fastify.get('/posts', feedController.getPosts);
   // POST /feed/post
-  fastify.post('/post', feedController.postPost);
+  fastify.post(
+    '/post',
+    {
+      schema: {
+        body: {
+          type: 'object',
+          properties: {
+            title: { type: 'string', minLength: 5 },
+            content: { type: 'string', minLength: 5 },
+          },
+          required: ['title', 'content'],
+        },
+      },
+    },
+    feedController.postPost
+  );
 }

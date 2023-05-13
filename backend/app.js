1a69fe403ac2e084312e729bf1102395787d81e3
nhamonin <nazarii.hamonin@gmail.com>
2023-05-13 23:09:36 +0300

    Add MongoDB connection and Post model schema

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,21 @@
 import fastify from 'fastify';
 import Cors from '@fastify/cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
 import { feedRoutes } from './routes/feed.js';
 
+dotenv.config();
+
 const app = fastify({ logger: true });
+
+try {
+  mongoose.connect(process.env.MONGODB_URI);
+  app.log.info('Connected to MongoDB');
+} catch (err) {
+  app.log.error(err);
+}
+
 app.register(Cors, { origin: '*' });
 app.register(feedRoutes, { prefix: '/feed' });
 
---
+++ backend/models/post.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    creator: {
+      type: Object,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Post = mongoose.model('Post', postSchema);

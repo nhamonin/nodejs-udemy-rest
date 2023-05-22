db20aa5bb10174729dbc1fb49a5115db5f4c314e
nhamonin <nazarii.hamonin@gmail.com>
2023-05-22 13:33:11 +0300

    Add login functionality to authRoutes and controllers/auth.js for user authentication and token generation

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 import { User } from '../models/user.js';
 
@@ -23,4 +24,38 @@ const signup = async (request, reply) => {
   };
 };
 
-export { signup };
+const login = async (request, reply) => {
+  const { email, password } = request.body;
+
+  const user = await User.findOne({ email: email });
+
+  if (!user) {
+    reply.code(401);
+    throw new Error('User does not exist!');
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordCorrect) {
+    reply.code(401);
+    throw new Error('Password is incorrect!');
+  }
+
+  const token = jwt.sign(
+    {
+      email: user.email,
+      userId: user._id.toString(),
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: '1h' }
+  );
+
+  reply.code(200);
+  reply.log.info('User logged in successfully!');
+  return {
+    token: token,
+    userId: user._id,
+  };
+};
+
+export { signup, login };
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,10 @@
-import { signup } from '../controllers/auth.js';
+import { signup, login } from '../controllers/auth.js';
 
 import { userSchema } from '../validation/userSchema.js';
 
 export async function authRoutes(fastify, options) {
   // PUT /auth/signup
   fastify.put('/signup', { schema: userSchema }, signup);
+  // POST /auth/login
+  fastify.post('/login', login);
 }

cb3e8eeb192a7956f51f02290df4ba1a20614ddd
nhamonin <nazarii.hamonin@gmail.com>
2023-05-24 13:28:32 +0300

    Add validator import and validate email and password inputs in createUser mutation

--- backend/graphql/resolvers.js
+++ backend/graphql/resolvers.js
@@ -1,3 +1,4 @@
+import validator from 'validator';
 import bcrypt from 'bcryptjs';
 
 import { User } from '../models/user.js';
@@ -13,6 +14,23 @@ const resolvers = {
   },
   Mutation: {
     createUser: async (_, args, req) => {
+      const errors = [];
+      if (!validator.isEmail(args.userInput.email)) {
+        errors.push({ message: 'E-Mail is invalid.' });
+      }
+      if (
+        validator.isEmpty(args.userInput.password) ||
+        !validator.isLength(args.userInput.password, { min: 5 })
+      ) {
+        errors.push({ message: 'Password too short!' });
+      }
+      if (errors.length > 0) {
+        const error = new Error('Invalid input.');
+        error.extensions = {
+          errors: errors.map((err) => ({ message: err.message })),
+        };
+        throw error;
+      }
       const existingUser = await User.findOne({ email: args.userInput.email });
 
       if (existingUser) {

1b21d4e1932e7342ca2f30508ff6bb8e3a128706
nhamonin <nazarii.hamonin@gmail.com>
2023-05-25 18:03:31 +0300

    Add user status API test and DB connection for Auth Controller in auth-controller.js

--- backend/test/auth-controller.js
+++ backend/test/auth-controller.js
@@ -1,8 +1,11 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 import { User } from '../models/user.js';
-import { login } from '../controllers/auth.js';
+import { login, getStatus } from '../controllers/auth.js';
 
 describe('Auth Controller - Login', () => {
   it('should throw an error with error 500 if accessing the database fails', async () => {
@@ -28,5 +31,35 @@ describe('Auth Controller - Login', () => {
       expect(reply.statusCode).to.equal(500);
       expect(err.message).to.equal('Internal Server Error');
     }
+    sinon.restore();
+  });
+
+  it('should send a response with a valid user status for an existing user', async () => {
+    const userId = '5c0f66b979af55031b34728a';
+    const user = new User({
+      email: 'test12345@gmail.com',
+      password: 'test12345',
+      name: 'Testqq',
+      posts: [],
+      _id: userId,
+    });
+
+    await mongoose.connect(process.env.MONGODB_URI_TEST);
+    await user.save();
+    const req = {
+      userId,
+      statusCode: 500,
+    };
+    const reply = {
+      code: function (code) {
+        this.statusCode = code;
+        return this;
+      },
+    };
+    const res = await getStatus(req, reply);
+    expect(reply.statusCode).to.equal(200);
+    expect(res.status).to.equal('I am new!');
+    await User.deleteMany({});
+    await mongoose.disconnect();
   });
 });

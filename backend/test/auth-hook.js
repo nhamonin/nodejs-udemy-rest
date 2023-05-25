4dd84bb8a386eb2a1d87b3d3d18b342ff10a940d
nhamonin <nazarii.hamonin@gmail.com>
2023-05-25 13:31:08 +0300

    Add tests for handling invalid authorization headers in isAuthenticated hook

--- backend/test/auth-hook.js
+++ backend/test/auth-hook.js
@@ -1,20 +1,59 @@
 import { expect } from 'chai';
 import isAuthenticated from '../hooks/is-auth.js';
 
-it('should throw an error if no authorization header is present', async () => {
-  const req = {
-    headers: {
-      authorization: null,
-    },
-  };
-  const reply = {
-    code: () => {},
-  };
+describe('isAuthenticated Hook', () => {
+  it('should throw an error if no authorization header is present', async () => {
+    const req = {
+      headers: {
+        authorization: null,
+      },
+    };
+    const reply = {
+      code: () => {},
+    };
 
-  try {
-    await isAuthenticated(req, reply);
-    throw new Error('Expected isAuthenticated to throw, but it did not.');
-  } catch (err) {
-    expect(err.message).to.equal('Not authenticated.');
-  }
+    try {
+      await isAuthenticated(req, reply);
+      throw new Error('Expected isAuthenticated to throw, but it did not.');
+    } catch (err) {
+      expect(err.message).to.equal('Not authenticated.');
+    }
+  });
+
+  it('should throw an error if the authorization header is only one string', async () => {
+    const req = {
+      headers: {
+        authorization: 'only-one-string',
+      },
+    };
+    const reply = {
+      code: () => {},
+    };
+
+    try {
+      await isAuthenticated(req, reply);
+      throw new Error('Expected isAuthenticated to throw, but it did not.');
+    } catch (err) {
+      expect(err.message).to.equal('Not authenticated.');
+    }
+  });
+
+  it('should throw an error if the authorization header is not a bearer token', async () => {
+    const req = {
+      headers: {
+        authorization: 'Bearer not-a-bearer-token',
+      },
+    };
+
+    const reply = {
+      code: () => {},
+    };
+
+    try {
+      await isAuthenticated(req, reply);
+      throw new Error('Expected isAuthenticated to throw, but it did not.');
+    } catch (err) {
+      expect(err.message).to.equal('Not authenticated.');
+    }
+  });
 });
